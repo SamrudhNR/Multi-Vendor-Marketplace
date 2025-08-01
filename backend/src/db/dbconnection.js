29d2a47f400df71bf62a240e335a445632fe3b91
samrudh <samrudh02@gmail.com>
2025-08-01 12:30:55 +0530

    connection to mongoDB atlas

---
+++ backend/src/db/dbconnection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';    
+dotenv.config();
+
+const connectDB= async () => {
+    try {
+        const connectionInstance=await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB connected successfully: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1); // Exit the process with failure
+    }
+}
+
+export default connectDB;
